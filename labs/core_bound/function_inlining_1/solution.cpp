750c2019ab0c52bc89c695bec1319c1cd0849be2
Julio Vidaurre <julio.vidaurre@gmail.com>
2025-05-10 20:54:32 -0400

    Custom qsort on core bound - function_inlining_1
    It's slightly faster than doing std::sort (about 3% faster).

--- labs/core_bound/function_inlining_1/solution.cpp
+++ labs/core_bound/function_inlining_1/solution.cpp
@@ -3,10 +3,26 @@
 #include <algorithm>
 #include <stdlib.h>
 
-static int compare(const void *lhs, const void *rhs) {
-  auto &a = *reinterpret_cast<const S *>(lhs);
-  auto &b = *reinterpret_cast<const S *>(rhs);
+// static int compare(const void *lhs, const void *rhs) {
+//   auto &a = *reinterpret_cast<const S *>(lhs);
+//   auto &b = *reinterpret_cast<const S *>(rhs);
 
+//   if (a.key1 < b.key1)
+//     return -1;
+
+//   if (a.key1 > b.key1)
+//     return 1;
+
+//   if (a.key2 < b.key2)
+//     return -1;
+
+//   if (a.key2 > b.key2)
+//     return 1;
+
+//   return 0;
+// }
+
+static int compare(S a, S b) {
   if (a.key1 < b.key1)
     return -1;
 
@@ -22,9 +38,37 @@ static int compare(const void *lhs, const void *rhs) {
   return 0;
 }
 
+static void swap(std::array<S, N> &arr, int i, int j) {
+  S temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp;
+}
+
+static int partition(std::array<S, N> &arr, int lo, int hi) {
+  int i = lo, j = hi + 1;
+  S &v = arr[lo];
+  while (true) {
+    while (compare(arr[++i], v) < 0) if (i == hi) break;
+    while (compare(v, arr[--j]) < 0) if (j == lo) break;
+    if (i >= j) break;
+    swap(arr, i, j);
+  }
+  swap(arr, lo, j);
+  return j;
+}
+
+static void custom_qsort(std::array<S, N> &arr, int lo, int hi) {
+  if (hi <= lo) return;
+
+  int j = partition(arr, lo, hi);
+  custom_qsort(arr, lo, j - 1);
+  custom_qsort(arr, j + 1, hi);
+}
+
 void solution(std::array<S, N> &arr) {
   // qsort(arr.data(), arr.size(), sizeof(S), compare);
-  std::sort(arr.begin(), arr.end(), [](S &a, S &b) {
-    return a.key1 < b.key1 || ((a.key1 == b.key1) && (a.key2 < b.key2));
-  });
+  // std::sort(arr.begin(), arr.end(), [](S &a, S &b) {
+  //   return a.key1 < b.key1 || ((a.key1 == b.key1) && (a.key2 < b.key2));
+  // });
+  custom_qsort(arr, 0, arr.size() - 1);
 }
