8beb904126f995a93fc264ae54f86a2a3ecaa7e6
Julio Vidaurre <julio.vidaurre@gmail.com>
2025-05-12 15:02:36 -0400

    Solve core bound - vectorization_2

--- labs/core_bound/vectorization_2/solution.cpp
+++ labs/core_bound/vectorization_2/solution.cpp
@@ -1,10 +1,15 @@
 #include "solution.hpp"
 
 uint16_t checksum(const Blob &blob) {
-  uint16_t acc = 0;
+  uint32_t sum = 0;
   for (auto value : blob) {
-    acc += value;
-    acc += acc < value; // add carry
+    sum += value;
   }
-  return acc;
+
+  while (sum >> 16) {
+    // Every value in the upper 16 bits is the sum of the carries accumulated.
+    sum = (sum & 0xFFFF) + (sum >> 16);
+  }
+
+  return sum;
 }
