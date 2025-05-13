a157aaa23dc09f3d93f1d820aedde4047a40db3c
Julio Vidaurre <julio.vidaurre@gmail.com>
2025-05-13 16:28:39 -0400

    Solve core bound - compiler_intrinsics_1

--- labs/core_bound/compiler_intrinsics_1/solution.cpp
+++ labs/core_bound/compiler_intrinsics_1/solution.cpp
@@ -1,6 +1,8 @@
 
 #include "solution.h"
 #include <memory>
+#include <smmintrin.h>
+
 
 void imageSmoothing(const InputVector &input, uint8_t radius,
                     OutputVector &output) {
@@ -21,7 +23,35 @@ void imageSmoothing(const InputVector &input, uint8_t radius,
   }
 
   // 2. main loop.
+  
   limit = size - radius;
+  for (; pos < limit - 8; pos += 8) {
+    __m128i simd_current_sum = _mm_set1_epi16(currentSum);
+
+    __m128i sub_u8 = _mm_loadu_si64(input.data() + (pos - radius - 1));
+    __m128i sub = _mm_cvtepu8_epi16(sub_u8);
+
+    __m128i add_u8 = _mm_loadu_si64(input.data() + pos + radius);
+    __m128i add = _mm_cvtepu8_epi16(add_u8);
+
+    __m128i diff = _mm_sub_epi16(add, sub);
+
+    // Compute the prefix sum in SIMD-way (by adding the shifted values).
+    diff = _mm_add_epi16(diff, _mm_slli_si128(diff, 2)); // shift the entire 128-bit register by 2 bytes (1 uint16 value)
+    diff = _mm_add_epi16(diff, _mm_slli_si128(diff, 4)); // shift the entire 128-bit register by 4 bytes (2 uint16 value)
+    diff = _mm_add_epi16(diff, _mm_slli_si128(diff, 8)); // shift the entire 128-bit register by 8 bytes (4 uint16 value)
+
+    // Store result into output
+    __m128i final_sum = _mm_add_epi16(simd_current_sum, diff);
+    _mm_storeu_si128((__m128i *)(output.data() + pos), final_sum);
+
+    // Set currentSum to the last computed value
+    int last_sum = _mm_extract_epi16(final_sum, 7);
+    currentSum = last_sum;
+  }
+  
+  
+  // 2.1 main loop - remaining cases of previous loop.
   for (; pos < limit; ++pos) {
     currentSum -= input[pos - radius - 1];
     currentSum += input[pos + radius];
