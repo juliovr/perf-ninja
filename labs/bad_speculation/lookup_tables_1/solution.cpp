31f73ce90ddab97468c5160e4f249b5481541b5b
Julio Vidaurre <julio.vidaurre@gmail.com>
2025-05-15 16:50:48 -0400

    Solve bad speculation - lookup_tables_1

--- labs/bad_speculation/lookup_tables_1/solution.cpp
+++ labs/bad_speculation/lookup_tables_1/solution.cpp
@@ -1,14 +1,21 @@
 #include "solution.hpp"
 
+
+static int buckets[] = {
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+  1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 
+  2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 
+  3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 
+  4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 
+  5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 
+  6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 
+};
+
 static std::size_t mapToBucket(std::size_t v) {
-                              //   size of a bucket
-  if      (v < 13)  return 0; //   13
-  else if (v < 29)  return 1; //   16
-  else if (v < 41)  return 2; //   12
-  else if (v < 53)  return 3; //   12
-  else if (v < 71)  return 4; //   18
-  else if (v < 83)  return 5; //   12
-  else if (v < 100) return 6; //   17
+  if (v < (sizeof(buckets) / sizeof(buckets[0]))) {
+    return buckets[v];
+  }
+
   return DEFAULT_BUCKET;
 }
 
