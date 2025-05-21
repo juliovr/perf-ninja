414b324fd5db20d36fe5591aa8b17f1e1a6fedaa
Julio Vidaurre <julio.vidaurre@gmail.com>
2025-05-20 20:16:47 -0400

    Solve io_opt1.
    baseline:
    --------------------------------------------------------
    Benchmark              Time             CPU   Iterations
    --------------------------------------------------------
    Small file/0        81.7 us         81.7 us        33968
    Medium file/1       38.8 ms         38.8 ms           72
    Large file/2        4.99 s          4.99 s             1
    
    modified:
    --------------------------------------------------------
    Benchmark              Time             CPU   Iterations
    --------------------------------------------------------
    Small file/0        24.7 us         24.1 us       107755
    Medium file/1       6.09 ms         6.08 ms          457
    Large file/2       0.763 s         0.763 s             4

--- labs/misc/io_opt1/solution.cpp
+++ labs/misc/io_opt1/solution.cpp
@@ -1,22 +1,17 @@
 #include "solution.hpp"
 
-#include <fstream>
-#include <stdexcept>
+#include "MappedFile.hpp"
 
 uint32_t solution(const char *file_name) {
-  std::fstream file_stream{file_name};
-  if (!file_stream.is_open())
-    throw std::runtime_error{"The file could not be opened"};
-
+  MappedFile mapped_file(file_name);
+  
   // Initial value has all bits set to 1
   uint32_t crc = 0xff'ff'ff'ff;
 
   // Update the CRC32 value character by character
+  const char *contents = mapped_file.getContents().data();
   char c;
-  while (true) {
-    file_stream.read(&c, 1);
-    if (file_stream.eof())
-      break;
+  while ((c = *contents++) != 0) {
     update_crc32(crc, static_cast<uint8_t>(c));
   }
 
